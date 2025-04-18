b672ee38c4b09c30fe82ef0a8b6317c38c92448b
Francisco <Francisco@gmail.com>
2025-04-18 17:46:16 +0200

    Category

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const categoryController = require('../controllers/categoryController');
+
+router.get('/', categoryController.getCategories);
+router.post('/', categoryController.createCategory);
--- services/composantsService.js
+++ services/composantsService.js
@@ -1,4 +1,11 @@
 const composant = require('../models/composantModel');
 
 module.exports.getComposants = async (query) => {
-    
+    try {
+        let composants = await composant.find({ category: query });
+        return composants;
+    } catch (err) {
+        console.error('Error fetching composants:', err);
+        return null;
+    }
+}
